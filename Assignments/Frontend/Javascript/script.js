fea8161eee13047d9f03f4b99870a447c4ccab10
Sudalaimani <sudalaimani5112007@gmail.com>
2026-02-06 09:22:58 +0530

    Add HTML and CSS files for Day 8 and Day 9 assignments, including structure and styling for CSS selectors and borders

--- Assignments/Frontend/Javascript/script.js
+++ Assignments/Frontend/Javascript/script.js
@@ -1,8 +1,12 @@
-console.log("Hello JS");
-console.log(1);
-console.log(true);
-console.log(false);
+// var (KW) variableName (Variable name) = (Assignment) value()
+
+let userName = "Sudalaiamani";
+console.log(typeof userName);
+let userStatus = "Student";
+let age  = 23;
+let userAdddress = `19/4, Ranasurettunayakkanpatti, Ettaiyapuram,Tuticorin`;
+let cgpa = 8.4;
+let graduation = 2026;
 
 
-
-
+console.log(`The user name is "${userName}", the user age is "${age}" the user status is "${userStatus}" the graduation year of the user is "${graduation}" the CGPA of the user is "${cgpa}" the user address is "${userAdddress}"`);
---
+++ Frontend/Javascript/Day8DataTypes/script.js
@@ -0,0 +1,39 @@
+// Data Types :-
+
+// 1)String :-
+// Group of Characters is known as string using "", '' and ``
+
+console.log("String");
+console.log('String');
+console.log(`String`);
+
+// Boolean :-
+console.log(true);
+console.log(false);
+
+// Number
+console.log(1);
+
+// Null :-
+let itemNull = null
+console.log(itemNull);
+
+// Undefined :-
+console.log(undefined);
+
+// Array :-
+console.log([]);
+
+// Object :-
+console.log({});
+
+
+// Variable Decalration :-
+let itemOne = 100;
+console.log(itemOne);
+
+
+function a () {
+    return "Hii"
+}
+console.log(typeof a);
