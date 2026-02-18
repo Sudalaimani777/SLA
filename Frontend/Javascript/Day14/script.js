d3f879ca8f6361f79e6222bdc3e21633f9807f48
Sudalaimani <sudalaimani5112007@gmail.com>
2026-02-18 09:07:42 +0530

    Add HTML and JavaScript files for Day 14 and Day 15, implementing condition statements and user status checks.

--- Frontend/Javascript/Day14/script.js
+++ Frontend/Javascript/Day14/script.js
@@ -1,5 +1,16 @@
-let a = "100";
-let b = "20";
+// Condition Statement :-
+const userAge = 20;
 
-let c  = a / b;
-console.log(c);
+// Ternary Operator
+const result = userAge >= 18 ? "User is eligible for voting" : "User is not eligible for voting";
+console.log(result);
+
+const studentMark = 81;
+
+const studentResult = studentMark < 25 ? "Fail" : studentMark >= 25 && studentMark < 50 ? "Good" : studentMark >= 51 && studentMark <= 80 ? "Wow" : studentMark >= 81 && studentMark <= 90 ? "Good Job Bruhh" : "Suuuuuper"
+
+console.log(studentResult);
+
+const userIdAndAge = 18 && true;
+
+if(userIdAndAge) console.log("Success");
---
+++ Frontend/Javascript/Day15/script.js
@@ -0,0 +1,25 @@
+// 1. Write a program that checks:If the user is logged in.If logged in, check if they are an admin or a normal user.
+
+let isLoggedIn = true;
+let isAdmin = false;
+
+function checkUserStatus(isLoggedIn, isAdmin) {
+    if (isLoggedIn) {
+        if (isAdmin) {
+            console.log("Welcome, Admin!");
+        } else {
+            console.log("Welcome, User!");
+        }
+    } else {
+        console.log("You are not logged in.");
+    }
+}
+checkUserStatus(isLoggedIn, isAdmin);
+
+// 2. Check the number is odd or even.
+const checkNumber = num => {
+    const result = num % 2 === 0 ? "Even" : "Odd";
+    console.log(`The number ${num} is ${result}.`)
+}
+checkNumber(10);
+checkNumber(7);
