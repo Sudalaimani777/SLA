e0e5707f6cebc75a22d429457ef58f7183b38c73
Sudalaimani <sudalaimani5112007@gmail.com>
2026-03-12 09:49:08 +0530

    feat: initialize React project with Vite, add basic components and styles
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application layout.
    - Implemented App component to include Header and Hero components.
    - Developed Header component with a simple title.
    - Created Hero component as a placeholder.
    - Added React logo SVG to assets directory.
    - Established global styles in index.css for consistent theming.
    - Set up main entry point in main.jsx to render the App component.
    - Configured Vite in vite.config.js for React support.

---
+++ Frontend/Javascript/Day28/script.js
@@ -0,0 +1,31 @@
+// setTimeout(() => {
+//     console.log("Starting...")
+// },1000)
+
+// setTimeout(() => {
+//     console.log("End")
+// },2000)
+
+// let timer = "start";
+
+// setTimeout(() => {
+//     if(timer === "start"){
+//         console.log("Starting");
+//         timer = "end"
+//     }else if(timer === "end"){
+//         setTimeout(() => {
+//             console.log("Ended")
+//         },2000)
+//     }
+// },1000)
+
+
+// if(timer === "start"){
+//     setTimeout(() => {
+//         console.log("Starting...");
+//         timer = "end"
+//         setTimeout(() => {
+//             console.log("Ended");
+//         }, 2000)
+//     }, 1000)
+// }
