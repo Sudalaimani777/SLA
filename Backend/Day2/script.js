3743f8ace872c48c723607350c84709eb367bd27
sudalamani777 <sudalaimani5112007@gmail.com>
2026-06-03 16:54:32 +0530

    feat: add additional functions and examples for user validation and data manipulation

--- Backend/Day2/script.js
+++ Backend/Day2/script.js
@@ -11,3 +11,21 @@ console.log(validUser(20));
 
 const findEvenNumbers = (...numbers) => numbers.filter(num => num % 2 === 0);
 console.log(findEvenNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+
+const inviteUser = (name, age, city, state) => `The user name is ${name}, his age was ${age}, he lived in ${city}, ${state}`
+console.log(inviteUser("Sudalaimani", 21, "Chennai", "Tamil Nadu"));
+
+const spreadExample = (data) => {
+    console.log("OG Data -> ", data)
+    const copiedData = {...data};
+    copiedData.course = "MERN";
+    return copiedData;
+    
+}
+
+const obj = {
+    course : "React",
+    duration: "2 Months"
+}
+
+console.log(spreadExample(obj))
